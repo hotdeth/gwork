f473aa3de7566794f9a8d803bd3b5a9fea52aea8
hotdeth <radhiox4@icloud.com>
2025-10-04 12:45:41 +0300

    Set controllers for tasks and auth

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,17 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\TaskController;
+
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function() {
+    Route::get('/tasks', [TaskController::class, 'index']);
+    Route::post('/tasks', [TaskController::class, 'store']);
+    Route::put('/tasks/{id}', [TaskController::class, 'update']);
+    Route::delete('/tasks/{id}', [TaskController::class, 'destroy']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
