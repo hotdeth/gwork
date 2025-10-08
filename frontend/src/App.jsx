8651df44ed79cbf46e11699a87da072deabd7ca1
Eredin <157521205+RidhaAlasadi@users.noreply.github.com>
2025-10-08 19:46:02 +0300

    adding Home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Button from "./components/Button"
-import Note from "./components/Note"
+import Home from "./components/Home.jsx"
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Button/>
-      <Note/>
+   <Home></Home> 
     </>
   )
 }
